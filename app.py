669639d66698d4ce087ea19e2790fc29a6f14a1b
syash7202 <workspacesyash7202@gmail.com>
2025-08-25 14:55:38 +0530

    test 101

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from ArgoCD Image Updater Demo!"
+    return "Hello from ArgoCD Image Updater Demo! hello from update!!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
